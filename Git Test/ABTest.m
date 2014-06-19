7ff7025b641b365110b2d0a2c7a8d6f8802333d8
Alex Bertzeletos <Alex@MacBook-Pro.local>
2014-06-19 20:30:46 +0200

    added a test class

---
+++ Git Test/ABTest.m
@@ -0,0 +1,13 @@
+//
+//  ABTest.m
+//  Git Test
+//
+//  Created by Alex Bertzeletos on 6/19/14.
+//  Copyright (c) 2014 Alex Bertzeletos. All rights reserved.
+//
+
+#import "ABTest.h"
+
+@implementation ABTest
+
+@end
